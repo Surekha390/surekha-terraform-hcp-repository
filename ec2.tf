bcc2c77bc47485b16372592274f4817dda99c3b5
Surekha <surekha.tallapalli@gmail.com>
2025-04-04 15:13:02 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-087f352c165340ea1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HCP EC2 instance"
+  }
+}
